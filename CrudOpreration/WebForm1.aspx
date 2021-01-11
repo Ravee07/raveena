a29b8b2326b1b6f68a096f189779ce0cabc66fe4
Ravee07 <77093484+Ravee07@users.noreply.github.com>
2021-01-11 11:58:32 +0530

    Add files via upload

---
+++ CrudOpreration/WebForm1.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PhoneBookAppp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                
+                <tr>
+                    <td>
+                        Name:<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <td>
+                        PhoneNum:<asp:TextBox ID="tctNum" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Address<asp:TextBox ID="txtAdd" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+
+
+
+
+                <tr>
+                    <td>
+                        <asp:Button ID="add" runat="server" Text="add" OnClick="add_Click"></asp:Button>
+                    </td>
+                
+
+
+
+
+                
+                    <td>
+                        <asp:Button ID="update" runat="server" Text="update" OnClick="update_Click"></asp:Button>
+
+                    </td>
+                
+
+
+                
+                    <td>
+                        <asp:Button ID="delete" runat="server" Text="delete" OnClick="delete_Click"></asp:Button>
+                    </td>
+
+                    <td>
+                    
+                    <asp:Button ID="DeleteAll" runat="server" Text="DeleteAll" OnClick="DeleteAll_Click" />
+                </td>
+
+                </tr>
+                <tr>
+
+                    <asp:GridView ID="GridView1" runat="server" style="margin-right: 22px"></asp:GridView>
+               
+                    
+                
+                </tr>
+
+               
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CrudOpreration/WebForm1.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PhoneBookAppp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=DESKTOP-OK1P17M\\MSSQLSERVERNEW;Initial Catalog=PhoneBookDB;Integrated Security=True";
+            con.Open();
+
+            if (!Page.IsPostBack)
+            {
+                DataShow();
+
+            }
+
+        }
+
+        protected void add_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Insert into TbPhoneBook(Name, PhoneNum,Address)values('" + txtName.Text.ToString() + "', '" + tctNum.Text.ToString() + "', '" + txtAdd.Text.ToString() + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+
+        }
+
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from TbPhoneBook";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+
+
+
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "update TbPhoneBook  set PhoneNum='" + tctNum.Text.ToString() + "' ,Address='" + txtAdd.Text.ToString() +"' where Name='"+txtName.Text.ToString()+"' ";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            //con.Close();
+            DataShow();
+        }
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from TbPhoneBook    where Name= '" + txtName.Text.ToString() + "'  ";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+
+        }
+
+        protected void DeleteAll_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = " delete  from TbPhoneBook ";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+             
+        }
+    }
+}

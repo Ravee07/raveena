859893712f8aa843ab8d741c9c6873f41df5eb38
Ravee07 <77093484+Ravee07@users.noreply.github.com>
2021-01-15 17:09:16 +0530

    Add files via upload

---
+++ Proj/empcontroller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using SampleProject.Models;
+using SampleProject.NewFolder;
+
+namespace SampleProject.Controllers
+{
+    public class empcontroller : Controller
+    {
+
+
+        NewFolder.UseDB use = new NewFolder.UseDB();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListEmployee()
+        {
+
+            DataSet ds = use.ListEmployee();
+            ViewBag.emp = ds.Tables[0];
+
+
+              return View();
+        }
+    }
+}

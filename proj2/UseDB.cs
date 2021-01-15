f0d487690360a1d35e5b7903f9f56003e8464602
Ravee07 <77093484+Ravee07@users.noreply.github.com>
2021-01-15 17:13:17 +0530

    Add files via upload

---
+++ proj2/UseDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Proj.NewFolder
+{
+    public class UseDB
+    {
+
+
+        SqlConnection con = new SqlConnection("Server = DESKTOP-OK1P17M\\MSSQLSERVERNEW; Database = EmployeeDB; integrated security =True");
+
+        public DataSet ListEmployee()
+        {
+            SqlCommand cmd = new SqlCommand("SP_ListEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            return ds;
+
+        }
+    }
+}
---
+++ proj2/empcontroller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Proj.Models;
+using Proj.NewFolder;
+
+namespace Proj.Controllers
+{
+    public class empcontroller : Controller
+    {
+
+
+        NewFolder.UseDB use = new NewFolder.UseDB();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListEmployee()
+        {
+
+            DataSet ds = use.ListEmployee();
+            ViewBag.emp = ds.Tables[0];
+
+
+            return View();
+        }
+    }
+}
+
+
+
+
+
